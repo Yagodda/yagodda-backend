e59705f3dbe25c04784bc1a376431628daec2d48
Artem Yakunin <artm.yakunin@gmail.com>
2022-04-06 16:17:08 +0700

    first commit

---
+++ src/main/java/com/yagodda/YagoddaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yagodda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagoddaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YagoddaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yagodda/YagoddaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagodda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagoddaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
